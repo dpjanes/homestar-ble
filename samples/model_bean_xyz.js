bad49707f1a9f8f0944819f9e3deb749b0bf4b3b
David Janes <davidjanes@davidjanes.com>
2015-02-22 06:43:50 -0500

    use new on() style

--- samples/model_bean_xyz.js
+++ samples/model_bean_xyz.js
@@ -11,11 +11,10 @@ var _ = module.homestar._;
 
 var wrapper = module.wrap("BeanXYZ");
 wrapper.on('model', function(model) {
-    console.log("HERE:XXX")
-    model.on_change(function(model) {
+    model.on("state", function(model) {
         console.log("+ state\n ", model.thing_id(), model.state());
     });
-    model.on_meta(function(model) {
+    model.on("meta", function(model) {
         console.log("+ meta\n ", model.thing_id(), _.ld.compact(model.meta().state()));
     });
     
